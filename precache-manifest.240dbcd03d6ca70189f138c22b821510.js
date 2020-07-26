c0e320cdaf0bab17bfd2e4e1e4372802056c2fed
Pidaparthi Mallikarjun <pidaparthimallikarjun1482001@gmail.com>
2020-07-26 19:40:40 +0530

    Updates

---
+++ precache-manifest.240dbcd03d6ca70189f138c22b821510.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aeba9d644e61bb750f578b77a6147a0",
+    "url": "/tictactoe/index.html"
+  },
+  {
+    "revision": "c9d81a2f6d0202818ffb",
+    "url": "/tictactoe/static/css/main.3dac82a3.chunk.css"
+  },
+  {
+    "revision": "5b73c48a47151b8079a0",
+    "url": "/tictactoe/static/js/2.2cdd2687.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tictactoe/static/js/2.2cdd2687.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c9d81a2f6d0202818ffb",
+    "url": "/tictactoe/static/js/main.3fce8ffd.chunk.js"
+  },
+  {
+    "revision": "a508da4123410c707990",
+    "url": "/tictactoe/static/js/runtime-main.b2f7fb25.js"
+  }
+]);
